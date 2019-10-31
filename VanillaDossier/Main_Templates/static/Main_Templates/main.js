b01d35712ec967da5c2c214d46926fb26e605b93
punctuationmarks <punctuaionmarks@protonmail.com>
2019-10-31 13:48:27 -0400

    Added color scheme change

--- VanillaDossier/Main_Templates/static/Main_Templates/main.js
+++ VanillaDossier/Main_Templates/static/Main_Templates/main.js
@@ -4,20 +4,30 @@
 // need to have this function on the actual html/css properties since
 // it needs to be an onClick() function
 
-let mainNav = document.getElementById('js_menu');
-let navBarToggle = document.getElementById('js_navbar_toggle');
+let htmlDoc = document.getElementById("html_id");
+let colorToggle = document.getElementById("js_color_scheme_toggle");
+let mainNav = document.getElementById("js_menu");
+let navBarToggle = document.getElementById("js_navbar_toggle");
 
 
-function hamburgerAnimation(mainNav) {
-    mainNav.classList.toggle("change_hamburger");
+
+// Color scheme toggle mode
+function darkLightModesSwitch(htmlDoc) {
+  htmlDoc.classList.toggle("dark_mode");
 }
 
-
-// this needs to be done separately, by the ID while the hamburgerAnimation is done by onClick
-navBarToggle.addEventListener('click', function() {
-    // mainNav.classList.toggle('change_hamburger');
-    mainNav.classList.toggle('active_navbar');
+colorToggle.addEventListener("click", function() {
+  htmlDoc.classList.toggle("dark_mode");
 });
 
+function hamburgerAnimation(mainNav) {
+  mainNav.classList.toggle("change_hamburger");
+}
+
+// this needs to be done separately, by the ID while the hamburgerAnimation is done by onClick
+navBarToggle.addEventListener("click", function() {
+  // mainNav.classList.toggle('change_hamburger');
+  mainNav.classList.toggle("active_navbar");
+});
 
 // free rides to the top
