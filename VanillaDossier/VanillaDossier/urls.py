547a3f1d3625345bf1c8a8b1df63eb9e5dc6d50f
punctuationmarks <punctuationmarks@protonmail.com>
2019-12-03 11:32:09 -0500

    tweaked logout

--- VanillaDossier/VanillaDossier/urls.py
+++ VanillaDossier/VanillaDossier/urls.py
@@ -10,7 +10,5 @@ urlpatterns = [
     path('', include('Main_Templates.urls')),
     path('dossiers/', include('Dossiers.urls')),
     path('ideas/', include('Ideas.urls')),
-    path('thoughts/', include('Thoughts.urls')),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-
+    path('thoughts/', include('Thoughts.urls'))
 ]
